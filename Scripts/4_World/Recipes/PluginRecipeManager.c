7e429b56a2ddd9865ca3c5b46011517eb4b61663
Dada <142039148+Dada09876@users.noreply.github.com>
2025-03-22 16:46:11 -0400

    Update PluginRecipeManager.c

---
+++ Scripts/4_World/Recipes/PluginRecipeManager.c
@@ -0,0 +1,13 @@
+// override the plugin recipe manager class
+modded class PluginRecipesManager
+{
+  // override the register recipes method
+  override void RegisterRecipies()
+  {
+    // call super so the "vanilla" code is called first
+    super.RegisterRecipies();
+    // an example of registering a recipe you created
+
+    //Tailored Clothes
+
+    RegisterRecipe(new Craft_tlr_PJTop);
