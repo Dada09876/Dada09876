ce04dfecc25b85604fe568e82ec82d4f0f6e6939
Dada <142039148+Dada09876@users.noreply.github.com>
2025-04-05 12:54:01 -0400

    Update TailoredClothes.c

--- Scripts/4_World/Recipes/Clothing/TailoredClothes.c
+++ Scripts/4_World/Recipes/Clothing/TailoredClothes.c
@@ -1,18 +1,13 @@
-class Craft_tlr_PJTop extends RecipeBase
+class Craft_tlr_PJTop extends Craft_Tailored_BaseRecipe
 {
 	override void Init()
 	{
 		m_Name = "Craft Pajama Top";
 
 		//conditions
-		m_MinDamageIngredient[0] = -1;//-1 = disable check
-		m_MaxDamageIngredient[0] = 3;//-1 = disable check
-
+		
 		m_MinQuantityIngredient[0] = -1;//-1 = disable check
 		m_MaxQuantityIngredient[0] = -1;//-1 = disable check
-
-		m_MinDamageIngredient[1] = -1;//-1 = disable check
-		m_MaxDamageIngredient[1] = 3;//-1 = disable check
 
 		m_MinQuantityIngredient[1] = 70;//-1 = disable check
 		m_MaxQuantityIngredient[1] = -1;//-1 = disable check
@@ -35,7 +30,7 @@ class Craft_tlr_PJTop extends RecipeBase
 
 		m_IngredientAddHealth[1] = 0;// 0 = do nothing
 		m_IngredientSetHealth[1] = -1; // -1 = do nothing
-		m_IngredientAddQuantity[1] = -100;// 0 = do nothing
+		m_IngredientAddQuantity[1] = -50;// 0 = do nothing
 		m_IngredientDestroy[1] = false;// false = do nothing
 		m_IngredientUseSoftSkills[1] = false;// set 'true' to allow modification of the values by softskills on this ingredient
 		//----------------------------------------------------------------------------------------------------------------------
