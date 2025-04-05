40ed8b2d7218e3de761336bc7d558466b66f63ee
Dada <142039148+Dada09876@users.noreply.github.com>
2025-04-05 12:58:04 -0400

    Update TailoredClothes.c

--- Scripts/4_World/Recipes/Clothing/TailoredClothes.c
+++ Scripts/4_World/Recipes/Clothing/TailoredClothes.c
@@ -38,3 +38,44 @@ class Craft_tlr_PJTop extends Craft_Tailored_BaseRecipe
 		//result1
 		AddResult("tlr_PJTop_");
 };
+
+class Craft_tlr_PJPants extends Craft_Tailored_BaseRecipe
+{
+	override void Init()
+	{
+		m_Name = "Craft Pajama Pants";
+
+		//conditions
+		
+		m_MinQuantityIngredient[0] = -1;//-1 = disable check
+		m_MaxQuantityIngredient[0] = -1;//-1 = disable check
+
+		m_MinQuantityIngredient[1] = 70;//-1 = disable check
+		m_MaxQuantityIngredient[1] = -1;//-1 = disable check
+		//----------------------------------------------------------------------------------------------------------------------
+
+		//INGREDIENTS
+		//ingredient 0
+		
+		InsertIngredient(0,"tlr_Fabric_Colorbase");
+
+		m_IngredientAddHealth[0] = 0;// 0 = do nothing
+		m_IngredientSetHealth[0] = -1; // -1 = do nothing
+		m_IngredientAddQuantity[0] = 0;// 0 = do nothing
+		m_IngredientDestroy[0] = true;//true = destroy, false = do nothing
+		m_IngredientUseSoftSkills[0] = false;// set 'true' to allow modification of the values by softskills on this ingredient
+
+		//ingredient 1
+		
+		InsertIngredient(0,"sewingkit");
+
+		m_IngredientAddHealth[1] = 0;// 0 = do nothing
+		m_IngredientSetHealth[1] = -1; // -1 = do nothing
+		m_IngredientAddQuantity[1] = -50;// 0 = do nothing
+		m_IngredientDestroy[1] = false;// false = do nothing
+		m_IngredientUseSoftSkills[1] = false;// set 'true' to allow modification of the values by softskills on this ingredient
+		//----------------------------------------------------------------------------------------------------------------------
+
+		//result1
+		AddResult("tlr_PJPants_");
+};
